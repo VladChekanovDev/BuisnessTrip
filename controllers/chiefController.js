ce40501a39ffd45424bddfca852b6370ffd3bcbf
Vlad C <02chekanovvlad@gmail.com>
2021-12-03 00:37:46 +0300

    Added worker's registration by a chief
    Chief can registrate workers now

--- controllers/chiefController.js
+++ controllers/chiefController.js
@@ -1,3 +1,5 @@
+import md5 from 'md5';
+
 import User from '../models/User.js';
 import Chief from "../models/Chief.js";
 import Worker from "../models/Worker.js";
@@ -20,8 +22,54 @@ export const getWorkers = async(req, res) => {
         const workers = await department.getWorkers();
         res.render('chief/workers', {
             pageTitle: 'Рабочие',
-            workers: workers
+            workers: workers,
+            userId: userId
         });
+    } catch(e) {
+        console.log(e);
+        res.redirect('/error/500');
+    }
+};
+
+export const getAddWorker = async(req, res) => {
+    const userId = req.params.userId;
+    res.render('chief/add-worker', {
+        pageTitle: 'Добавление рабочего',
+        userId: userId
+    });
+};
+
+export const postAddWorker = async(req, res, next) => {
+    try {
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const patronymic = req.body.patronymic;
+        const dateOfBirth = req.body.dateOfBirth;
+        const dateOfHiring = req.body.dateOfHiring;
+        const login = req.body.login;
+        const password = md5(req.body.password);
+        const position = req.body.position;
+        const imageURL = req.body.imageURL;
+        const salary = req.body.salary;
+        const userId = req.body.userId;
+        const user = await User.findByPk(userId);
+        const chief = await user.getChief();
+        const department = await chief.getDepartment();
+        await User.registrateWorker({
+            login: login,
+            password: password,
+            userType: 'worker'
+        },{
+            firstName: firstName,
+            lastName: lastName,
+            patronymic: patronymic,
+            dateOfBirth: dateOfBirth,
+            dateOfHiring: dateOfHiring,
+            position: position,
+            imageURL: imageURL,
+            salary: salary
+        }, department.id);
+        res.redirect(`/chief/${userId}/workers`);
     } catch(e) {
         console.log(e);
         res.redirect('/error/500');
--- routes/chiefRouter.js
+++ routes/chiefRouter.js
@@ -6,6 +6,10 @@ const chiefRouter = Router();
 
 chiefRouter.get('/:userId/workers', chiefController.getWorkers);
 
+chiefRouter.get('/:userId/workers/add-worker', chiefController.getAddWorker);
+
+chiefRouter.post('/workers/add-worker', chiefController.postAddWorker);
+
 chiefRouter.get('/:userId', chiefController.getChief);
 
 export default chiefRouter;
