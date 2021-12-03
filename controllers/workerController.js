5f55342809bbe58ce07f25bd69f744c104a37a23
Vlad C <02chekanovvlad@gmail.com>
2021-12-04 00:29:31 +0300

    Added accept and refuse trip functionality
    Worker can see their buisness trip and accept or refuse them

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -1,8 +1,49 @@
 import User from "../models/User.js";
 import Department from "../models/Department.js";
+import BuisnessTrip from "../models/BuisnessTrip.js";
 
 export const getWorker = (req, res, next) => {
+    const userId = req.params.userId;
     res.render('worker/worker', {
-        pageTitle: 'Панель рабочего'
-    })
+        pageTitle: 'Панель рабочего',
+        userId: userId
+    });
+};
+
+export const getTrips = async(req, res) => {
+    const userId = req.params.userId;
+    const user = await User.findByPk(userId);
+    const worker = await user.getWorker();
+    const buisnessTrips = await worker.getBuisnessTrips();
+    res.render('worker/buisness-trips', {
+        pageTitle: 'Мои командировки',
+        userId: userId,
+        buisnessTrips: buisnessTrips
+    });
+};
+
+export const postAcceptTrip = async(req, res) => {
+    const userId = req.body.userId;
+    const buisnessTripId = req.body.buisnessTripId;
+    await BuisnessTrip.update({
+        status: 'Принято'
+    }, {
+        where: {
+            id: buisnessTripId
+        }
+    });
+    res.redirect(`/worker/${userId}/buisness-trips`);
+};
+
+export const postRefuseTrip = async(req, res) => {
+    const userId = req.body.userId;
+    const buisnessTripId = req.body.buisnessTripId;
+    await BuisnessTrip.update({
+        status: 'Откланено'
+    }, {
+        where: {
+            id: buisnessTripId
+        }
+    });
+    res.redirect(`/worker/${userId}/buisness-trips`);
 }
--- routes/workerRouter.js
+++ routes/workerRouter.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import * as workerController from '../controllers/workerController.js';
+
 const workerRouter = Router();
+
+workerRouter.get('/:userId', workerController.getWorker);
+
+workerRouter.get('/:userId/buisness-trips', workerController.getTrips);
+
+workerRouter.post('/buisness-trips/accept-trip', workerController.postAcceptTrip);
+
+workerRouter.post('/buisness-trips/refuse-trip', workerController.postRefuseTrip);
 
 export default workerRouter;
