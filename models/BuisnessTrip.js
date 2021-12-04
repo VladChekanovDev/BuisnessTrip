39f4ee0012f7b03b272108b77162d20577dec151
Vlad C <02chekanovvlad@gmail.com>
2021-12-05 00:58:03 +0300

    Fixed update BuisnessTrip bug

--- models/BuisnessTrip.js
+++ models/BuisnessTrip.js
@@ -40,48 +40,53 @@ const BuisnessTrip = sequelize.define('buisnessTrip', {
 }, {
     getterMethods: {
         getDay() {
-            return this.orderDate.substr(9,2);
+            return (this.orderDate) ? this.orderDate.substr(9,2) : '';
         },
         getMonth() {
-            switch(this.orderDate.substr(5,2)) {
-                case '01': return 'января'
-                case '02': return 'февраля'
-                case '03': return 'марта'
-                case '04': return 'апреля'
-                case '05': return 'мая'
-                case '06': return 'июня'
-                case '07': return 'июля'
-                case '08': return 'августа'
-                case '09': return 'сентября'
-                case '10': return 'октября'
-                case '11': return 'ноября'
-                case '12': return 'декабря'
-            }
+            if (this.orderDate) {
+                switch(this.orderDate.substr(5,2)) {
+                    case '01': return 'января'
+                    case '02': return 'февраля'
+                    case '03': return 'марта'
+                    case '04': return 'апреля'
+                    case '05': return 'мая'
+                    case '06': return 'июня'
+                    case '07': return 'июля'
+                    case '08': return 'августа'
+                    case '09': return 'сентября'
+                    case '10': return 'октября'
+                    case '11': return 'ноября'
+                    case '12': return 'декабря'
+                    default: return ''
+                }
+            } else return
         },
         getYear() {
-            return this.orderDate.substr(2,2);
+            return (this.orderDate) ? this.orderDate.substr(2,2) : '';
         },
         getBeginDay() {
-            return this.dateOfBegin.substr(9,2);
+            return (this.dateOfBegin) ? this.dateOfBegin.substr(9,2) : '';
         },
         getBeginMonth() {
-            switch(this.dateOfBegin.substr(5,2)) {
-                case '01': return 'января'
-                case '02': return 'февраля'
-                case '03': return 'марта'
-                case '04': return 'апреля'
-                case '05': return 'мая'
-                case '06': return 'июня'
-                case '07': return 'июля'
-                case '08': return 'августа'
-                case '09': return 'сентября'
-                case '10': return 'октября'
-                case '11': return 'ноября'
-                case '12': return 'декабря'
-            }
+            if (this.dateOfBegin) {
+                switch(this.dateOfBegin.substr(5,2)) {
+                    case '01': return 'января'
+                    case '02': return 'февраля'
+                    case '03': return 'марта'
+                    case '04': return 'апреля'
+                    case '05': return 'мая'
+                    case '06': return 'июня'
+                    case '07': return 'июля'
+                    case '08': return 'августа'
+                    case '09': return 'сентября'
+                    case '10': return 'октября'
+                    case '11': return 'ноября'
+                    case '12': return 'декабря'
+                }                
+            } else return ''
         },
         getBeginYear() {
-            return this.dateOfBegin.substr(2,2);
+            return (this.dateOfBegin) ? this.dateOfBegin.substr(2,2) : '';
         }
     }
 });
