56319b22fff5bf453e9fb1352b1e9264a66a31f1
Vlad C <02chekanovvlad@gmail.com>
2021-12-06 01:55:07 +0300

    Almost implemented add-worker form

--- public/js/script.js
+++ public/js/script.js
@@ -6,4 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(elems);
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(elems, {format: 'yyyy-mm-dd'});
+});
